4060890c48bec2ab41050ce6fd29710b770d7aa4
s6s9h <s6s9h@ugrad.cs.ubc.ca>
2024-04-02 22:07:22 -0700

    Updated CLI for UML class diagram and update CitationButton to use citation instead of InputPersistence

--- src/main/ui/gui/CitationButton.java
+++ src/main/ui/gui/CitationButton.java
@@ -1,6 +1,6 @@
 package ui.gui;
 
-import model.InputPersistence;
+import model.Citation;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,10 +8,10 @@ import java.awt.*;
 // represents a Button, which represents a CitationButton
 public class CitationButton extends JButton {
     public static final String SELECT_CITATION = "select citation";
-    private final InputPersistence citation;
+    private final Citation citation;
 
     //EFFECTS: creates a citation button with the given citation
-    public CitationButton(InputPersistence citation) {
+    public CitationButton(Citation citation) {
         super(citation.toString());
         setBackground(null);
 
@@ -21,7 +21,7 @@ public class CitationButton extends JButton {
         setPreferredSize(new Dimension(getWidth(), 100));
     }
 
-    public InputPersistence getCitation() {
+    public Citation getCitation() {
         return citation;
     }
 
